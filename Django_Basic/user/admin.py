691666cfea18475da268d01dbb17783e6df1846e
gabang2 <offbeat1020@naver.com>
2023-07-11 23:51:39 +0900

    __str__함수, Model의 verbose_name지정, admin.py

--- Django_Basic/user/admin.py
+++ Django_Basic/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from user.models import FcUser
+
+
+class FcUserAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'password')
+
+
+admin.site.register(FcUser, FcUserAdmin)
--- Django_Basic/user/models.py
+++ Django_Basic/user/models.py
@@ -7,5 +7,10 @@ class FcUser(models.Model):
     password = models.CharField(max_length=64, verbose_name="비밀번호")
     registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name="등록 시간")
 
+    def __str__(self):
+        return self.user_name
+
     class Meta:
         db_table = 'fastcampus_fcuser' # 앱들과 구분해서 db의 실제 테이블명을 짓기 위해서 사용함
+        verbose_name = "패스트캠퍼스 사용자"
+        verbose_name_plural = "패스트캠퍼스 사용자들"
