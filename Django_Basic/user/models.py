7ecfe01fabd352cb11800c0e09bcf54cc2187668
gabang2 <offbeat1020@naver.com>
2023-07-11 23:37:55 +0900

    makemigrations와 migrate

--- Django_Basic/user/models.py
+++ Django_Basic/user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class FcUser(models.Model):
-    user_name = models.CharField(max_length=64, verbose_name="사용자 이름")
+    user_name = models.CharField(max_length=32, verbose_name="사용자 이름")
     password = models.CharField(max_length=64, verbose_name="비밀번호")
     registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name="등록 시간")
 
