36aa7b3ce1151101b1502ac4553ad4bc39117fb9
gabang2 <offbeat1020@naver.com>
2023-07-12 00:44:15 +0900

    form페이지 정리 & views.py파일 세분화

--- Django_Basic/user/views.py
+++ Django_Basic/user/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def register(request):
-    return render(request, 'user/register.html')
+    # 1. url로 들어올 때
+    if request.method == 'GET':
+        return render(request, 'user/register.html')
+    # 2. submit(등록)버튼을 눌러서 들어올 때
+    elif request.method == 'POST':
+        return render(request, 'user/register.html')
+
