d390c7f58ad2120daee4b7f1fba34d3404140e1f
gabang2 <offbeat1020@naver.com>
2023-07-12 10:25:47 +0900

    session을 활용한 로그인 구현

--- Django_Basic/user/views.py
+++ Django_Basic/user/views.py
@@ -2,7 +2,7 @@ import re
 
 from django.contrib.auth.hashers import make_password, check_password
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from user.models import FcUser
@@ -45,13 +45,21 @@ def login(request):
         user_name = request.POST.get("user_name", None)
         password = request.POST.get("password", None)
 
-        if not(user_name and password):
+        if not (user_name and password):
             res_data['error'] = "모든 값을 입력해야 합니다."
         else:
             fcuser = FcUser.objects.get(user_name=user_name)
             if check_password(password, fcuser.password):
-                # 비밀번호가 일치, 오류 처리를 하자
-                pass
+                request.session['user'] = fcuser.id
+                return redirect("/")
             else:
                 res_data['error'] = "비밀번호가 일치하지 않습니다."
         return render(request, 'login.html', res_data)
+
+
+def home(request):
+    user_id = request.session.get('user')
+    if user_id:
+        fcuser = FcUser.objects.get(pk=user_id)
+        return HttpResponse(fcuser.user_name)
+    return HttpResponse("<h1>Home</h1>")
